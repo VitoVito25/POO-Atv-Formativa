1396c8c69cded655e51e3bfd16e60912b7da5435
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-04 13:42:49 -0300

    calcularPagamentoMensal()

--- Financiamento.java
+++ Financiamento.java
@@ -1,8 +1,21 @@
 public class Financiamento {
 
-    private doubela valorImovel;
-    prazoFinanciamento
-
-    valorImovel (double), prazoFinanciamento (int) e taxaJurosAnual (double).
+    private double valorImovel;
+    private double prazoFinanciamento; // Em anos
+    private double taxaJurosAnual;
     
+
+    public Financiamento(double valorImovel, double prazoFinanciamento, double taxaJurosAnual ) {
+        this.valorImovel = valorImovel;
+        this.prazoFinanciamento = prazoFinanciamento;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+
+    public double calcularPagamentoMensal() {
+        double prazoFinanciamentoMeses = this.prazoFinanciamento * 12;
+        double taxaJurosMensal = this.taxaJurosAnual / 12;
+        double valorPagamentoMensal = this.valorImovel / prazoFinanciamentoMeses * (1 + taxaJurosMensal);
+
+        return valorPagamentoMensal;
+    }
 }

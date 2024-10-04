42f8a6577167f9c3f02e3f8f8ca4b0f91ce3bb92
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-04 14:06:19 -0300

    solicitaValorImovel() e documentação

--- Financiamento.java
+++ Financiamento.java
@@ -1,16 +1,29 @@
+/**
+ * Classe de financiamento
+ * 
+ * @author Victor Renaud
+ * @version 1.0
+ */
 public class Financiamento {
 
     private double valorImovel;
     private double prazoFinanciamento; // Em anos
     private double taxaJurosAnual;
     
-
+    // Constructor
     public Financiamento(double valorImovel, double prazoFinanciamento, double taxaJurosAnual ) {
         this.valorImovel = valorImovel;
         this.prazoFinanciamento = prazoFinanciamento;
         this.taxaJurosAnual = taxaJurosAnual;
     }
 
+    /**
+     * Metodo para calcular o pagamento mensal
+     * @return Retorna o valor do pagamento mensal do financiamento
+     * 
+     * @author Victor Renaud
+     * @version 1.0
+     */
     public double calcularPagamentoMensal() {
         double prazoFinanciamentoMeses = this.prazoFinanciamento * 12;
         double taxaJurosMensal = this.taxaJurosAnual / 12;
@@ -19,6 +32,13 @@ public class Financiamento {
         return valorPagamentoMensal;
     }
 
+    /**
+     * Metodo para calcular o valor total que deve ser pago do imovel
+     * @return Retorna o valor total que dever ser pago do imovel
+     * 
+     * @author Victor Renaud
+     * @version 1.0
+     */
     public double totalPagamento() {
         double valorPagamentoMensal = calcularPagamentoMensal();
         double prazoFinanciamentoMeses = this.prazoFinanciamento * 12;
--- InterfaceUsuario.java
+++ InterfaceUsuario.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Classe para funções de interface do usuario
+ * 
+ * 
+ * @author Victor Renaud
+ * @version 1.0
+ */
 public class InterfaceUsuario {
-    
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                // Comando para limpar o console no Windows
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // Comando para limpar o console no Unix/Linux/MacOS
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao limpar o console.");
+            e.printStackTrace();
+        }
+    }
+
+    public double solicitaValorImovel(){
+
+        System.out.println("Insira o valor total do Imovel: ");
+        double valorImovel = this.scanner.nextDouble();
+
+        return valorImovel;
+    }
 }

e0268ed3c6e1fc57353319d8291cfab9fbbe1857
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-18 15:36:25 -0300

    Update FinanciamentoApartamento.java

--- src/modelo/FinanciamentoApartamento.java
+++ src/modelo/FinanciamentoApartamento.java
@@ -18,6 +18,24 @@ public class FinanciamentoApartamento extends Financiamento{
         this.numeroAndar = numeroAndar;
     }
 
+    // Getters
+    public int getVagasGaragem() {
+        return vagasGaragem;
+    }
+
+    public int getNumeroAndar() {
+        return numeroAndar;
+    }
+
+    // Setters
+    public void setVagasGaragem(int vagasGaragem) {
+        this.vagasGaragem = vagasGaragem;
+    }
+
+    public void setNumeroAndar(int numeroAndar) {
+        this.numeroAndar = numeroAndar;
+    }
+
     /**
      * Metodo para calcular o pagamento mensal com o sistema de amortização PRICE
      * @return Retorna o valor do pagamento mensal do financiamento
@@ -54,4 +72,25 @@ public class FinanciamentoApartamento extends Financiamento{
     public double calcularTotalPagamento() {
         return (this.calcularPagamentoMensal() * this.calcularPrazoMeses());
     }
+
+    /**
+     * Metodo imprimir os detalhes do financiamento na tela
+     * 
+     * 
+     * @author Victor Renaud
+     * @version 1.0
+     * @Overwrite
+     */
+    public void imprimirDetalhesFinanciamento() {
+        System.out.println("#---- Detalhes do Financiamento Apartamento ----#");
+        System.out.printf("Andar: %.2f m²\n", this.getNumeroAndar());
+        System.out.printf("Vagas na garagem: %.2f m²\n", this.getVagasGaragem());
+        System.out.printf("Valor do Imóvel: R$ %.2f\n", this.getValorImovel());
+        System.out.printf("Prazo do Financiamento: %.1f anos\n", this.getPrazoFinanciamento());
+        System.out.printf("Taxa de Juros Anual: %.2f%%\n", this.getTaxaJurosAnual());
+        System.out.printf("Valor do Pagamento Mensal: R$ %.2f\n", this.calcularPagamentoMensal());
+        System.out.printf("Valor Total a ser Pago: R$ %.2f\n", this.calcularTotalPagamento());
+        System.out.println("#----------------------------------#");
+    }
+    
 }

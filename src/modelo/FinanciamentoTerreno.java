accf2c9807267754d7c7e8c2d312082c953eee26
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-18 15:37:37 -0300

    Update FinanciamentoTerreno.java

--- src/modelo/FinanciamentoTerreno.java
+++ src/modelo/FinanciamentoTerreno.java
@@ -15,6 +15,16 @@ public class FinanciamentoTerreno extends Financiamento{
         this.tipoZona = tipoZona;
     }
 
+    // Getter
+    public String getTipoZona() {
+        return tipoZona;
+    }
+
+    // Setter
+    public void setTipoZona(String tipoZona) {
+        this.tipoZona = tipoZona;
+    }
+
     /**
      * Metodo para calcular o pagamento total com o adicional de 2% para risco de inadimplência 
      * @return Retorna o valor do pagamento mensal do financiamento
@@ -25,5 +35,24 @@ public class FinanciamentoTerreno extends Financiamento{
      */
     public double calcularTotalPagamento() {
         return (super.calcularTotalPagamento() * 1.02);
+    }
+
+    /**
+     * Metodo imprimir os detalhes do financiamento na tela
+     * 
+     * 
+     * @author Victor Renaud
+     * @version 1.0
+     * @Overwrite
+     */
+    public void imprimirDetalhesFinanciamento() {
+        System.out.println("#---- Detalhes do Financiamento Terreno ----#");
+        System.out.printf("Tipo da zona do terreno: %.2f m²\n", this.getTipoZona());
+        System.out.printf("Valor do Imóvel: R$ %.2f\n", this.getValorImovel());
+        System.out.printf("Prazo do Financiamento: %.1f anos\n", this.getPrazoFinanciamento());
+        System.out.printf("Taxa de Juros Anual: %.2f%%\n", this.getTaxaJurosAnual());
+        System.out.printf("Valor do Pagamento Mensal: R$ %.2f\n", this.calcularPagamentoMensal());
+        System.out.printf("Valor Total a ser Pago: R$ %.2f\n", this.calcularTotalPagamento());
+        System.out.println("#----------------------------------#");
     }
 }
